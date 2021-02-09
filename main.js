7fdf7f43dd885bd3a22430f01da4df938b1bded7
fahim <fahimmontasirshakil@gmail.com>
2021-02-09 14:56:27 +0600

    done

---
+++ main.js
@@ -0,0 +1,6 @@
+document.getElementById('open-menu').addEventListener('click',()=>{
+    document.getElementById('menu-items').style.display= 'block';
+})
+document.getElementById('exit-btn').addEventListener('click',()=>{
+    document.getElementById('menu-items').style.display= 'none';
+})
